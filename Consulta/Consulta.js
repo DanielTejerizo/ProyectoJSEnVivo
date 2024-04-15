5f3d769e7af28fa31fe69bf11fac43da7bc8fb65
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-04-15 13:01:15 +0200

    .

--- Consulta/Consulta.js
+++ Consulta/Consulta.js
@@ -34,12 +34,12 @@ function crearCabecera() {
 }
 
 function elegir() {
-  
-  let botonTitulo=document.getElementById("botonTitulo");
-  let botonAutor=document.getElementById("botonAutor");
-  let botonAnio=document.getElementById("botonAnio");
-  let botonGenero=document.getElementById("botonGenero");
- 
+
+  let botonTitulo = document.getElementById("botonTitulo");
+  let botonAutor = document.getElementById("botonAutor");
+  let botonAnio = document.getElementById("botonAnio");
+  let botonGenero = document.getElementById("botonGenero");
+
   botonAnio.addEventListener("click", conseguirDatos); //evento para que al hacer click
   botonTitulo.addEventListener("click", conseguirDatos); //evento para que al hacer click
   botonAutor.addEventListener("click", conseguirDatos); //evento para que al hacer click
@@ -60,9 +60,9 @@ function conseguirDatos() {
 
 function tratarDatos(datos) {
   let anio = document.getElementById("anio").value; //valor del input
-  let titulo=document.getElementById("titulo").value; //valor del input
-  let autor=document.getElementById("autor").value; //valor del input
-  let genero=document.getElementById("genero").value; //valor del input
+  let titulo = document.getElementById("titulo").value; //valor del input
+  let autor = document.getElementById("autor").value; //valor del input
+  let genero = document.getElementById("genero").value; //valor del input
   let tabla = document.getElementById("libros"); //tabla
   tabla.hidden = false; //mostrar tabla
   let tbody = document.getElementById("tbody"); //seleccionar el tbody
@@ -70,88 +70,9 @@ function tratarDatos(datos) {
 
   datos.libros.forEach((libro) => {
     //forEach
-    if (libro.anio_publicacion <= parseInt(anio)) {
-      //creo celdas
-      let fila = document.createElement("tr");
-      let celda1 = document.createElement("td");
-      let celda2 = document.createElement("td");
-      let celda3 = document.createElement("td");
-      let celda4 = document.createElement("td");
-      let celda5 = document.createElement("td");
-      let boton = document.createElement("button");
-      boton.id = "boton";
-      boton.append("Ver");
-      boton.addEventListener("click", function () {
-        crearImagen(libro); //imagen del libro
-      });
-      celda5.append(boton);
-
-      celda1.append(libro.titulo); //en la celda 1 poner el titulo del libro
-      celda2.append(libro.autor); //en la celda 2 poner el autor del libro
-      celda3.append(libro.anio_publicacion); //en la celda 3 poner el año de publicacion del libro
-      celda4.append(libro.genero); //en la celda 4 poner el genero del libro
-      fila.append(celda1); //añadir celdas a las filas
-      fila.append(celda2);
-      fila.append(celda3);
-      fila.append(celda4);
-      fila.append(celda5);
-
-      tbody.append(fila); //añadir fila al tbody
-    }else if (libro.titulo == titulo){
-      //creo celdas
-      let fila = document.createElement("tr");
-      let celda1 = document.createElement("td");
-      let celda2 = document.createElement("td");
-      let celda3 = document.createElement("td");
-      let celda4 = document.createElement("td");
-      let celda5 = document.createElement("td");
-      let boton = document.createElement("button");
-      boton.id = "boton";
-      boton.append("Ver");
-      boton.addEventListener("click", function () {
-        crearImagen(libro); //imagen del libro
-      });
-      celda5.append(boton);
-
-      celda1.append(libro.titulo); //en la celda 1 poner el titulo del libro
-      celda2.append(libro.autor); //en la celda 2 poner el autor del libro
-      celda3.append(libro.anio_publicacion); //en la celda 3 poner el año de publicacion del libro
-      celda4.append(libro.genero); //en la celda 4 poner el genero del libro
-      fila.append(celda1); //añadir celdas a las filas
-      fila.append(celda2);
-      fila.append(celda3);
-      fila.append(celda4);
-      fila.append(celda5);
-
-      tbody.append(fila); //añadir fila al tbody
-    }else if (libro.autor == autor){
-      //creo celdas
-      let fila = document.createElement("tr");
-      let celda1 = document.createElement("td");
-      let celda2 = document.createElement("td");
-      let celda3 = document.createElement("td");
-      let celda4 = document.createElement("td");
-      let celda5 = document.createElement("td");
-      let boton = document.createElement("button");
-      boton.id = "boton";
-      boton.append("Ver");
-      boton.addEventListener("click", function () {
-        crearImagen(libro); //imagen del libro
-      });
-      celda5.append(boton);
-
-      celda1.append(libro.titulo); //en la celda 1 poner el titulo del libro
-      celda2.append(libro.autor); //en la celda 2 poner el autor del libro
-      celda3.append(libro.anio_publicacion); //en la celda 3 poner el año de publicacion del libro
-      celda4.append(libro.genero); //en la celda 4 poner el genero del libro
-      fila.append(celda1); //añadir celdas a las filas
-      fila.append(celda2);
-      fila.append(celda3);
-      fila.append(celda4);
-      fila.append(celda5);
-
-      tbody.append(fila); //añadir fila al tbody
-    }else if (libro.genero == genero){
+    if (
+        (titulo ==="" || libro.titulo === titulo) && (autor ==="" || libro.autor===autor) && (anio==="" || libro.anio===anio) && (genero==="" || libro.genero===genero)
+    ) {
       //creo celdas
       let fila = document.createElement("tr");
       let celda1 = document.createElement("td");
@@ -195,8 +116,8 @@ function crearImagen(libro) {
     imagen.src = libro.imagen; //src
     imagen.alt = libro.imagen; //alt
     divImagen.append(imagen); //Meter imagen
-  }else{
-    let aviso=document.createElement("p") //Mensaje de aviso no salida
+  } else {
+    let aviso = document.createElement("p") //Mensaje de aviso no salida
     aviso.textContent("Sin imagen") //Texto
   }
 
